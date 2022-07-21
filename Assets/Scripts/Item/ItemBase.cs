cc4af4bfe0d361692ca00b986ab7bd5a9625474d
yamasyun <syuntastg44@gmail.com>
2022-07-21 23:34:25 +0900

    アイテム周り進行中

--- Assets/Scripts/Item/ItemBase.cs
+++ Assets/Scripts/Item/ItemBase.cs
@@ -6,11 +6,25 @@ public class ItemBase : MonoBehaviour
 {
     public bool isUse = false;
     public ItemScriptableData itemData;
+    public ItemList list;
     public int itemAmount;
 
     public virtual void Use(PlayerStatus status)
     {
         Debug.Log("アイテムを使用しました。");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+
+    public virtual void Get()
+    {
+
     }
 
     public virtual void Consumption()
