fa797d80ce1da0abdfdabb10f99ee375bc7f09f4
yamasyun <syuntastg44@gmail.com>
2022-07-21 23:33:27 +0900

    Excelファイルをスクリプタブルデータにする仕組みを入れた

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//$$REGION GLOBAL_ENUM$$
+//$$REGION_END GLOBAL_ENUM$$
+//$$REGION TABLE$$
+/// <summary>
+/// Global: ScriptableObject
+/// </summary>
+public class Global : ScriptableObject
+{
+//$$REGION ENUM$$
+//$$REGION_END ENUM$$
+//$$REGION CONST$$
+//$$REGION_END CONST$$
+//$$REGION CLASS$$
+ //$$REGION_END CLASS$$
+//$$REGION CODE$$
+ //$$REGION_END CODE$$
+}
+//$$REGION_END TABLE$$
