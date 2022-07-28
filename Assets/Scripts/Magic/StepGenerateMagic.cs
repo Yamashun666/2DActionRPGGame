e31f76d45263b433a3d8aebb7ee1aabce58941aa
yamasyun <syuntastg44@gmail.com>
2022-07-28 21:46:32 +0900

    地形生成魔法を作った。

--- Assets/Scripts/Magic/StepGenerateMagic.cs
+++ Assets/Scripts/Magic/StepGenerateMagic.cs
@@ -5,37 +5,25 @@ namespace ClearSky
 {
     public class StepGenerateMagic : MagicBase
     {
-        [SerializeField] StepController StepPreview;
-        public SimplePlayerController simple;
-        public bool isPreview = false; // プレビューモードが有効かどうか
-        public Vector3 preview = default; // 足場を置くプレビュー時の座標を宣言
-        public Vector3 direction;
-        // Start is called before the first frame update
+        [SerializeField] GameObject generateStep;
+        [SerializeField] Transform GeneratePos = null;
+        [SerializeField] SimplePlayerController simple;
+
         public override void OnCall()
         {
-            if (IsValid) // 魔法使用後なら早期リターンをして処理を行わない。
+            if (IsValid)
             {
                 return;
             }
-            if (simple.isStepGenerate)
+            if (!simple.isStepGenerate)
             {
-                StepGeneratePreview();
-                base.OnCall();
+                GameObject stepGenerate = Instantiate(generateStep, GeneratePos.position, Quaternion.identity);
+                Debug.Log("fuck");
             }
-
-            
-        }
-
-        public void StepGeneratePreview()
-        {
-            isPreview = true; //プレビュー関数が呼ばれたらTrueを返す
-            preview = Input.mousePosition; // マウス座標を取得
-            direction = transform.position;
-            direction = Camera.main.ScreenToWorldPoint(preview);
-            StepController step = Instantiate(StepPreview, direction, Quaternion.identity); //プレビューモード用のprefabを呼ぶ
-
-            step.transform.position = direction;
+            base.OnCall();
         }
     }
-
 }
+
+
+
