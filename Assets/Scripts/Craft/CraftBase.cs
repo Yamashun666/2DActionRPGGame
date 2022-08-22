4de279aba6ebcfd8c496f9b46224431014d210ca
yamasyun <syuntastg44@gmail.com>
2022-08-23 02:57:31 +0900

    金床に接触しているときにボタンを押したらメニューを出るようにした

--- Assets/Scripts/Craft/CraftBase.cs
+++ Assets/Scripts/Craft/CraftBase.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CraftBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerStatus itemList; // 現在取得しているアイテム一覧を参照
+    private int itemNum; // 必要なアイテム数
+    [SerializeField] GameObject craftUI; //金床側にクラフトメニューをアタッチできるようにする
+
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("fuck");
+            CraftManu();
+        }
+    }
+    //金床がプレイヤーと接触した場合かつ、LeftShiftキーが押された場合、クラフトメニューが開かれる
+
+
+    private void CraftManu()
+    {
+        craftUI.gameObject.SetActive(true); //OntriggerEnter2Dしたら、クラフトメニューを表示する。
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Craft(ItemBase item)
     {
-        
+        itemList.HaveItems.Add(item); // Listにアイテムを追加する
+        InventoryUI.Instance.OnListView(itemList.HaveItems); // インベントリを更新する
     }
 }
